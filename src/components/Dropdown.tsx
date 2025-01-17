ffb2a422556011804e337fec5279d4cdc9978dad
brewla.design@gmail.com <brewla.design@gmail.com>
2025-01-17 15:51:04 -0800

    corrected dropdown component colors

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -19,7 +19,7 @@ export default function Dropdown({ label, options }: DropdownProps) {
             onMouseEnter={() => setIsOpen(true)}
             onMouseLeave={() => setIsOpen(false)}
         >
-            <button className="group flex items-center gap-2 w-full px-4 py-2 rounded-md bg-transparent hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none">
+            <button className="group flex items-center gap-2 w-full px-4 py-2 rounded-md bg-transparent hover:bg-gray-700 focus:outline-none">
                 {label}
                 <svg
                     width="16"
@@ -27,7 +27,7 @@ export default function Dropdown({ label, options }: DropdownProps) {
                     viewBox="0 0 16 16"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    className={`transform fill-black dark:fill-white transition-transform duration-200 ${isOpen ? "rotate-180" : "rotate-0"
+                    className={`transform fill-white transition-transform duration-200 ${isOpen ? "rotate-180" : "rotate-0"
                         }`}
                 >
                     <path
@@ -39,14 +39,14 @@ export default function Dropdown({ label, options }: DropdownProps) {
 
             <div
                 className={`${isOpen ? "scale-100 opacity-100" : "scale-95 opacity-0"
-                    } absolute right-0 mt-2 w-48 rounded-lg shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5 transform transition-all duration-200 origin-top-right`}
+                    } absolute right-0 mt-2 w-48 rounded-lg shadow-lg bg-gray-700 ring-1 ring-black ring-opacity-5 transform transition-all duration-200 origin-top-right`}
             >
                 <div className="py-1">
                     {options.map((option, index) => (
                         <a
                             key={index}
                             href={option.link}
-                            className="block px-4 py-2 text-sm text-black dark:text-white hover:bg-gray-50 hover:dark:bg-gray-600"
+                            className="block px-4 py-2 text-sm text-white hover:bg-gray-600"
                         >
                             {option.label}
                         </a>
